33ac629ca26942d84c862327f9e68f8b0e3dd15d
Minhaz Uddin <minhazrock1@gmail.com>
2025-10-27 17:42:45 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 console.log('i am minhaz');
 
-async function showWeather(){
-    let latitude = 15.3333;
-    let longitude = 74.0833;
+const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
 
-    const response = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+async function showWeather(){
+    // let latitude = 15.3333;
+    // let longitude = 74.0833;
+    let city = "goa";  
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+
+    const data = await response.json();
+    console.log("Weather data:-> " + data);
+
+    let newPara =  document.createElement('p');
+
+    newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`
+
+    document.body.appendChild(newPara); 
 
     
 }
