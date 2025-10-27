216cd60aa40f510b91f80b364dd568b2d37c480f
Minhaz Uddin <minhazrock1@gmail.com>
2025-10-27 12:32:57 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('i am minhaz');
+
+async function showWeather(){
+    let latitude = 15.3333;
+    let longitude = 74.0833;
+
+    const response = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+
+    
+}
