b224bce03a07b7a5edf6e4e5b34fea2e1df95529
Minhaz Uddin <minhazrock1@gmail.com>
2025-10-28 22:50:26 +0530

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log('i am minhaz');
 
 const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
 
-async function showWeather(){
+async function fetchWeatherDetails(){
     // let latitude = 15.3333;
     // let longitude = 74.0833;
 
@@ -14,13 +14,13 @@ async function showWeather(){
     const data = await response.json();
     console.log("Weather data:-> " , data);
 
-    let newPara =  document.createElement('p');
-    newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`
+    // let newPara =  document.createElement('p');
+    // newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`
 
-    document.body.appendChild(newPara); 
+    // document.body.appendChild(newPara); 
         
     } catch (err) {
-        
+        // handlle the error here
     }
 
    
