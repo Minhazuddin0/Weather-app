02e91775b2a12f171c4045099bb77e43eb07e74e
Minhaz Uddin <minhazrock1@gmail.com>
2025-11-26 18:04:37 +0530

    update

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ const grantAccesContainer = document.querySelector(".grant-location-container");
 const searchForm = document.querySelector("[data-searchForm]");
 const loadingScreen = document.querySelector(".loading-container");
 const userInfoContainer = document.querySelector(".user-info-container");
+
+// initialy variable needs
+
+let currentTab = userTab;
+const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
+currentTab.classList.add("current-tab");
+
+
+// ek kaam pending hai ???
+
+
