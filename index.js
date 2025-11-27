aad78557d4c3c98c31d18f575ddc72449f023685
Minhaz Uddin <minhazrock1@gmail.com>
2025-11-27 12:03:15 +0530

    update

--- index.js
+++ index.js
@@ -8,23 +8,35 @@ const userInfoContainer = document.querySelector(".user-info-container");
 
 // initialy variable needs
 
-let currentTab = userTab;
+let oldTab = userTab;
 const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
-currentTab.classList.add("current-tab");
+oldTab.classList.add("current-tab");
 
 
 // ek kaam pending hai ???
 
 function switchTab(clickedTab){
-    if(clickedTab != currentTab){
-        currentTab.classList.remove("current-tab");
-        currentTab = clickedTab; 
-        currentTab.classList.add("current-tab");
+    if(newTab != oldTab){
+        oldTab.classList.remove("current-tab");
+        oldTab = newTab; 
+        oldTab.classList.add("current-tab");
 
         if(!searchForm.classList.contain("active")) {
+            // kya search form wala is visible, if yes then make it visible 
             userInfoContainer.classList.remove("active");
             grantAccessContainer.classList.remove("active");
             searchForm.classList.add("active"); 
+        }
+        else{
+            
+            // main pehle search wale tab par tha, ab your weather tab visible karna hai
+
+            searchForm,classList.remove("active"); 
+            userInfoContainer.classList.remove("active");
+            
+            // ab main your weather tab mai aa gya hu, to weather bho display karna pdega, so lets check local storage firt for coordinates, if we have saved them there
+
+            getfromSessionStorage ();
         }
     }
 }
