d2af293b5255fd26a7da63f50d825b87059ed19c
Minhaz Uddin <minhazrock1@gmail.com>
2025-12-17 20:32:55 +0530

    update

--- index.js
+++ index.js
@@ -116,10 +116,10 @@ function renderWeatherInfo(weatherInfo){
     desc.innerText = weatherInfo?.weather?.[0]?.description;
     weatherIcon.src = `https://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
 
-    temp.innerText = `${weatherInfo?.main?.temp} °C`;
-    windspeed.innerText = weatherInfo?.wind?.speed;
-    humidity.innerText = weatherInfo?.main?.humidity;
-    cloudiness.innerText = weatherInfo?.clouds?.all;
+    temp.innerText = `${weatherInfo?.main?.temp} °C `;
+    windspeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
+    humidity.innerText = `${weatherInfo?.main?.humidity}%`;
+    cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;
 
 }
 
